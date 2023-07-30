fade0e22da0570240fbb99d6f28e7006692f33e6
Ery <48968656+eryellima@users.noreply.github.com>
2023-07-30 01:03:56 -0300

    Update client.py

--- gRPC/client.py
+++ gRPC/client.py
@@ -3,12 +3,16 @@ import champion_pb2
 import champion_pb2_grpc
 
 def run():
+    # Cria um canal de comunicação gRPC seguro para o servidor na porta localhost:50051.
     channel = grpc.insecure_channel('localhost:50051')
+   
+    # Cria um stub (cliente) para o serviço ChampionService usando o canal criado acima.
     stub = champion_pb2_grpc.ChampionServiceStub(channel)
 
     # Solicitar informações de um campeão por nome
     campeao = input("Digite o nome do campeão que deseja consultar: ")
-
+    # Chama o método remoto GetChampionByName do serviço ChampionService,
+    # passando uma instância de ChampionNameRequest contendo o nome do campeão desejado.
     response = stub.GetChampionByName(champion_pb2.ChampionNameRequest(name=campeao))
     if response.name:
         print("Nome:", response.name)
