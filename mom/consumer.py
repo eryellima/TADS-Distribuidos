50fffebbd1eca7cf8cfdbac4b75c67f0f8e76c3b
Ery <48968656+eryellima@users.noreply.github.com>
2023-07-30 00:55:02 -0300

    Add files via upload

--- mom/consumer.py
+++ mom/consumer.py
@@ -1,19 +1,29 @@
 import pika
 
-# Establish a connection to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+# Cria uma conex]ão bloqueante com o servidor
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+# Cria um canal na conexão
 channel = connection.channel()
 
-# Declare a queue
-queue_name = 'test_queue'
-channel.queue_declare(queue=queue_name)
+# Declara a fila de mensagens
+channel.queue_declare(queue='fila')
 
-# Define a callback function for handling messages
+# defeina a função que cserá chamada quando uma mensagem for recebida da fila
 def callback(ch, method, properties, body):
-    print("Received message: {}".format(body.decode()))
+    print("Mensagem recebida:", body.decode())
 
-# Consume messages from the queue
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-print('Waiting for messages...')
+# Configura o consumidor para rebeber mensagem da fila, quando uma mensagem chega, a função 'callback' será chamada para processa-la e o parametro 'auto_ack=True' indica que as mensagens será automaticamente confirmada após ser consumidas
+channel.basic_consume(queue='fila',
+                      on_message_callback=callback, auto_ack=True)
+
+# Printa mensagem enquanto agurda as mensagens
+print("Aguardando mensagens...")
+
+# Inicia  o consumo de mensagens. O consumidor fica em execu~çao indefinidamente até que seja interrompido manualmente. Quando uma mensagen chegar na fila, a função 'callback' será chamada.
 channel.start_consuming()
+
+# Fecha a conexão com o servidor
+connection.close()
--- mom/producer.py
+++ mom/producer.py
@@ -1,16 +1,34 @@
 import pika
 
-# Establish a connection to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+
+# Cria uma conexão bloqueante com o servidor RabbitMQ que está executando no localhost
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+# Cria um canal na conexão
 channel = connection.channel()
 
-# Declare a queue
-queue_name = 'test_queue'
-channel.queue_declare(queue=queue_name)
+#Declara a fila chamada 'fila'
+channel.queue_declare(queue='fila')
 
-# Publish a message to the queue
-message = 'Hello, World!'
-channel.basic_publish(exchange='', routing_key=queue_name, body=message)
+# Envia mensagens para a fila
+def publicador():
+    while True:
+        mensagem = input("Digite uma mensagem ou 'sair' para encerrar o programa: ")
 
-print("Sent message: {}".format(message))
+        if mensagem == "sair":
+            break
+
+        # publica a mensagem na fila declarada
+        # O parâmetro 'exchange' é vazio, então a mensagem será enviada diretamente para a fila especificada em 'routing_key'
+        channel.basic_publish(
+            exchange='', routing_key='fila', body=mensagem)
+        
+    print("Publicador encerrado.")
+
+# Verifica se o script está sendo executado diretamente e não sendo importado como um módulo
+if __name__ == "__main__":
+    publicador()
+
+# Fecha a conexão com o servidor RabbitMQ    
 connection.close()
